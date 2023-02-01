b5cfa44ea8a4fa74e1e858a079825a5ff8a2befe
BVH18112003 <huybvph27872@fpt.edu.vn>
2023-02-01 16:11:13 +0700

    143_The_new _at_Method

---
+++ 11/143_The_new _at_Method/main.js
@@ -0,0 +1,9 @@
+const arr = [18,20,11];
+console.log(arr[0]);
+console.log(arr.at(0));
+// getting last array element
+console.log(arr[arr.length-1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+console.log('huy'.at(0));
+console.log('huy'.at(-1));
